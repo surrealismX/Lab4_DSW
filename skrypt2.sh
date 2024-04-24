2a59738c5df2c03e25f0de5bd1a3c6aabb529173
surrealismX <parkhomenko.oleh23@gmail.com>
2024-04-24 19:18:01 +0200

    Dodano aliasy dla flag

---
+++ skrypt2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+show_date() {
+    date
+}
+
+create_logs() {
+    local count=${1:-100}
+    for ((i=1; i<=$count; i++)); do
+        echo "Log $i, Created by: $0, Date: $(date)" > log${i}.txt
+    done
+}
+
+case "$1" in
+    --date | -d)
+        show_date
+        ;;
+    --logs | -l)
+        create_logs "$2"
+        ;;
+    --help | -h)
+        echo "Usage: $0 [--date|-d | --logs|-l | --help|-h]"
+        ;;
+    *)
+        echo "Unknown option: $1. Use --help for usage."
+        exit 1
+        ;;
+esac
